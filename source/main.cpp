947d030b54728f65f1bf70d39095fcb1869ce79b
rmdyh <rmdyh@hotmail.com>
2019-06-25 23:07:05 +0800

    add comments in code

--- source/main.cpp
+++ source/main.cpp
@@ -9,8 +9,8 @@
 #include"pagerank.h"
 
 static double now_value[BLOCK_SIZE];
-std::map<int,int> id2loc;
-std::set<int> nodeset,allnodeset;
+std::map<int,int> id2loc;   /*map web_id to its order in old.bin*/ 
+std::set<int> nodeset,allnodeset;/*maintain the web_id that has been processed*/
 
 int main(int argc, char** argv){
     if(split_data(argc,argv)==0)
@@ -37,6 +37,7 @@ int split_data(int argc,char ** argv){
     }
     int src,dest;
     id2loc.clear();
+    /*map web_id to its order in old.bin*/
     while(fscanf(fp,"%d%d",&src,&dest)!=EOF){
         if(id2loc.count(src)==0){
             id2loc[src]=id2loc.size();
@@ -51,6 +52,7 @@ int split_data(int argc,char ** argv){
     nodeset.clear();
     allnodeset.clear();
     fseek(fp,0,SEEK_SET);
+    /* break dataset into blocks and init the r_old */
     while(fscanf(fp,"%d%d",&src,&dest)!=EOF){
         if(allnodeset.count(src)==0){
             nodeset.insert(src);
@@ -62,15 +64,22 @@ int split_data(int argc,char ** argv){
             allnodeset.insert(dest);
             fwrite(&init,sizeof(double),1,old);
         }
+        /* up to max block-size */
         if(nodeset.size()>=BLOCK_SIZE){
+            /* 
+              nodeset.size() may be either BLOCK_SIZE or BLOCK_SIZE+1
+              Need to cope with the latter case
+              by recording 'dest' , erasing it in nodeset
+              and adding it latter.
+            */
             int backup=0;
             if(nodeset.size()>BLOCK_SIZE){
                 backup=dest;
                 nodeset.erase(dest);
             }
-            //record lseek
-            //read the file from head to tail
-            //and write the matrix in matrix.bin
+            // 1. record lseek
+            // 2. read the file from head to tail
+            //    and write one line of the sparse matrix in matrix.bin
             long pos=ftell(fp);
             fseek(fp,0,SEEK_SET);
             LinkTable lt={0};
@@ -78,6 +87,7 @@ int split_data(int argc,char ** argv){
             while(fscanf(fp,"%d%d",&src,&dest)!=EOF){
                 if(lt.pointer==0)
                     lt.pointer=src;
+                // write this line to matrix.bin
                 if(src!=lt.pointer){
                     if(lt.dest_num!=0){
                         lt.pointer=id2loc[lt.pointer];
@@ -86,30 +96,32 @@ int split_data(int argc,char ** argv){
                     }
                     lt={src,0,0};
                 }
+                // add new dest
                 ++lt.degree;
                 if(nodeset.count(dest)!=0){
                     node[lt.dest_num]=id2loc[dest];
                     ++lt.dest_num;
                 }
             }
+            // write this line to matrix.bin
             if(lt.dest_num!=0){
                 lt.pointer=id2loc[lt.pointer];
                 fwrite(&lt,sizeof(LinkTable),1,matrix);
                 fwrite(node,sizeof(int)*lt.dest_num,1,matrix);
             }
+            // block end with triple 0
             lt={0,0,0};
             fwrite(&lt,sizeof(LinkTable),1,matrix);
             fseek(fp,pos,SEEK_SET);
             nodeset.clear();
+            // add 'dest' to nodeset if erase it before
             if(backup){
                 nodeset.insert(backup);
             }
         }
     }
+    /* save the last block to matrix.bin */
     if(nodeset.size()>0){
-        //record lseek
-        //read the file from head to tail
-        //and write the matrix in matrix.bin
         fseek(fp,0,SEEK_SET);
         LinkTable lt={0};
         ToNode node;
@@ -151,20 +163,25 @@ int split_data(int argc,char ** argv){
 int solve(){
     FILE *matrix=fopen("matrix.bin","rb");
     FILE *old=fopen("old.bin","rb+"),*_new=fopen("new.bin","wb+");
+    /* get the number of webs */
     fseek(old,0,SEEK_END);
     int item_size=ftell(old)/sizeof(double);
     int t=0;
+    /* iteration start */
     while(true){
-        double total=.0;
+        double total=.0; 
         fseek(old,0,SEEK_SET);
         fseek(_new,0,SEEK_SET);
         fseek(matrix,0,SEEK_SET);
+        /* calculate r_new block by block */
         for(int begin=0;begin<item_size;begin+=BLOCK_SIZE){
             memset(now_value,0,sizeof(now_value));
             LinkTable lt;
             ToNode node;
+            /* read sparse matrix by line */
             while(fread(&lt,sizeof(LinkTable),1,matrix)){
                 if(lt.pointer==0){
+                /* hit the end of the block */
                     break;
                 }
                 fread(node,sizeof(int)*lt.dest_num,1,matrix);
@@ -177,6 +194,7 @@ int solve(){
             }
             int end=begin+BLOCK_SIZE<item_size?begin+BLOCK_SIZE:item_size;
             for(int i=0;i<end-begin;i++){
+                /* calculate the sum of pagerank values*/
                 total+=now_value[i];
             }
             fwrite(now_value,sizeof(double),end-begin,_new);
@@ -186,20 +204,23 @@ int solve(){
         double offset=0;
         fseek(old,0,SEEK_SET);
         fseek(_new,0,SEEK_SET);
+        /* normalize the r_new and write them into old.bin */
         for(int begin=0;begin<item_size;begin+=BLOCK_SIZE/2){
             int now_size=item_size-begin<BLOCK_SIZE/2?item_size-begin:BLOCK_SIZE/2;
             fread(now_value,sizeof(double),now_size,_new);
             fread(now_value+BLOCK_SIZE/2,sizeof(double),now_size,old);
             for(int i=0;i<now_size;++i){
-                now_value[i]+=total;
+                now_value[i]+=total; /* normalize */
+                /* calculate the difference of r_old and r_new */
                 offset+=fabs(now_value[i]-now_value[i+BLOCK_SIZE/2]);
             }
             fseek(old,sizeof(double)*begin,SEEK_SET);
+            /* write them into old.bin */
             assert(fwrite(now_value,sizeof(double),now_size,old));
             fflush(old);
         }
-        //printf("%f\n",offset);
         if(offset<EPSILON){
+            /* convergence */
             break;
         }
         t++;
@@ -222,6 +243,7 @@ int output(){
     FILE *result=fopen("result.txt","w");
     double value;
     int id=0;
+    // use priority_queue to fine top x website
     while(fread(&value,sizeof(double),1,old)){
         p.push(std::make_pair(++id,value));
     }
@@ -230,8 +252,10 @@ int output(){
         std::pair<int,double> tmp=p.top();
         p.pop();
         int web_id;
+        // get web_id from index.bin
         fseek(index,(tmp.first-1)*sizeof(int),SEEK_SET);
         fread(&web_id,sizeof(int),1,index);
+        // output the result to result.txt
         fprintf(result,"%d\t%.10f\n",web_id,tmp.second);
     }
     return 1;
--- source/pagerank.h
+++ source/pagerank.h
@@ -2,21 +2,21 @@
 #define _PAGERANK_
 #include<stdint.h>
 
-#define BLOCK_SIZE 10000
-#define EPSILON 1e-10
-#define BETA 0.85
-#define TOP 100
+#define BLOCK_SIZE 10000    /*max size of r_new's block*/
+#define EPSILON 1e-10       /*condition of convergance*/
+#define BETA 0.85           /*teleports*/
+#define TOP 100             /*the number of webs have highest pagerank in result.txt*/
 
 typedef struct{
-    long pointer;
-    uint16_t degree;
-    uint16_t dest_num;
+    long pointer;           /*point to the location of r_old*/
+    uint16_t degree;        /*degree*/
+    uint16_t dest_num;      /*the number of records in this block*/
 } LinkTable;
 
-typedef int ToNode[1<<16];
+typedef int ToNode[1<<16];  /*records in one LinkTable*/
 
-int split_data(int,char **);
-int solve();
-int output();
+int split_data(int,char **);/*break dataset into several blocks*/
+int solve();                /*calculate the pagerank of each web*/
+int output();               /*output the webs have highest pagerank into result.txt*/
 
 #endif
